747eb328a67be262187968e259163269f5ee3d0d
Adrian Markefka <am97veb@gmail.com>
2025-02-06 10:32:57 +0100

    add Athor page

---
+++ src/features/author/index.js
@@ -0,0 +1,21 @@
+import { Container } from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+const Author = () => (
+  <Container>
+    <Header
+      title="O autorze"
+    />
+    <Section
+      title="Adrian Markefka"
+      sectionBody={
+        <>
+          Nazywam się Adrian Markefka i mam 26 lat. Pochodzę ze Świerklańca, a obecnie mieszkam w Piekarach Śląskich (woj. Śląskie). Jestem absolwentem AWF Katowice, gdzie ukończyłem studia licencjckie z Bezpieczeństwa Wewnętrznego i studia magisterskie z Zarządzania. Obecnie pracuję na stanowisku protokolanta sądowego w Sądzie Apelacyjnym w Katowicach. W ostatnim czasie zainteresowałem się tematyką programowania, a po darmowym Livie YouCode zdecydowałem się rozpocząć do kursu Frontend Developera.
+        </>
+      }
+    />
+  </Container>
+);
+
+export default Author;
