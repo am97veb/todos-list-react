1bcc4c1bf970321a43d135ef349336a9d215202e
Adrian Markefka <am97veb@gmail.com>
2025-02-10 12:53:45 +0100

    adding new page to app (TaskDetailsPage

---
+++ src/features/tasks/TaskDetailsPage/index.js
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import { Container } from "../../../common/Container";
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
+import { useSelector } from "react-redux";
+import { selectTaskById } from "../tasksSlice";
+
+const TaskDetailsPage = () => {
+  const { id } = useParams();
+  const task = useSelector(state => selectTaskById(state, id));
+
+  return (
+    <Container>
+      <Header
+        title="Szczegóły zadania"
+      />
+      <Section
+        title={
+          task ? task.content : "Wybacz, nie ma takiego zadania😕"
+        }
+        sectionBody={
+          task &&
+          <>
+            <strong>Ukończono:</strong>
+            {" "}
+            {task.done ? "Tak" : "Nie"}
+          </>
+        }
+      />
+    </Container>
+  );
+}
+
+export default TaskDetailsPage;
