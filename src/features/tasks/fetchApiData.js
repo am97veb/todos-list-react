829afd01fffc2dec805f458efa55812153c7bfd6
Adrian Markefka <am97veb@gmail.com>
2025-02-12 12:00:13 +0100

    use of axios instead of fetch

--- src/features/tasks/fetchApiData.js
+++ src/features/tasks/fetchApiData.js
@@ -2,13 +2,6 @@ import axios from "axios";
 import { sourceApiData } from "./sourceApiData";
 
 export const fetchApiData = async () => {
-    // const response = await axios.get(sourceApiData);
-    // return await response.data;
-    const response = await fetch(sourceApiData);
-  
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    };
-  
-    return await response.json();
+    const response = await axios.get(sourceApiData);
+    return await response.data;
 }; 
