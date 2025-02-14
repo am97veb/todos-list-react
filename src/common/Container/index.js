48e83e08bb4670a5c9e7020d99d38f52dfd34a56
Adrian Markefka <am97veb@gmail.com>
2025-02-14 12:19:28 +0100

    less padding in the mobile view whole app

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -4,4 +4,8 @@ export const Container = styled.main`
     max-width: 1200px;
     margin: 0 auto;
     padding: 0 20px;
+
+    @media (max-width: ${({theme})=> theme.breakpoints.mobile}px) {
+        padding: 10px;
+    }
 `;
